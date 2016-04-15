4ef36d126425a79b465bbe121e5aa51a3e1ba572
Aurora49 <sliman.desmars@epitech.eu>
2016-04-15 04:30:15 -0700

    Ajout de la connexion

--- app/components/connexion/connexionController.js
+++ app/components/connexion/connexionController.js
@@ -1,6 +1,31 @@
 
 
-website.controller('connexionController', function($scope, $http) {
+website.controller('connexionController', function($scope, $http, $window) {
+
+  $scope.connexion = {
+      email : '',
+      password : '',
+    };
+
+    $scope.connexionAction = function(){
+
+        data = {
+          email : $scope.connexion.email,
+          password : CryptoJS.SHA1($scope.connexion.password).toString()
+        };
+
+        $http.post(url + '/api/connection', data).then(function(){
+
+          /*
+          * @TODO : check plus profondément la response pour être sur que la requête a bien été effectué
+          */
+          $window.location.href = '#/'
+        }, function(error){
+
+          alert("l'inscription n'a pas réussie");
+          $scope.message = 'Une erreur est survenue durant la connection';
+        });
+      };
 
   console.log("Controller Connexion");
 });
