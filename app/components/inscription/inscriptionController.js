90136a49cfe6a0cae9312ca18b86c36b9010abc6
Aurora49 <sliman.desmars@epitech.eu>
2016-04-15 03:38:02 -0700

    Correction de l'inscription

--- app/components/inscription/inscriptionController.js
+++ app/components/inscription/inscriptionController.js
@@ -2,4 +2,29 @@
 website.controller('inscriptionController', function($scope, $http) {
 
   console.log("Controller Inscription");
+
+  $scope.inscription = {
+      email : '',
+      password : '',
+    };
+
+    $scope.inscriptionAction = function(){
+
+        data = {
+          email : $scope.inscription.email,
+          password : CryptoJS.SHA1($scope.inscription.password).toString()
+        };
+
+        $http.post(url + '/api/inscription', data).then(function(){
+
+          //faire une alerte
+          //$scope.message = 'Connection réussi';
+          alert("Inscription réussie");
+
+        }, function(error){
+
+          alert("l'inscription n'a pas réussie");
+          $scope.message = 'Une erreur est survenue durant la connection';
+        });
+      };
 });
