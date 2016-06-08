fcc21ea240f9a0dd2ecb56e40748410bd09a8eaf
Nicolas Girard <nicolasg37@gmail.com>
2016-06-07 19:50:19 -0700

    UX improvements

--- app/components/main/mainController.js
+++ app/components/main/mainController.js
@@ -5,12 +5,16 @@ website.controller('mainController', function($scope, $http) {
 
   $scope.leftMenu = true;
   $scope.rightMenu = true;
-
   $http.get(url + '/api/applications').then(function(response){
 
-      $scope.data = response.data.Applications;
+  $scope.data = response.data.Applications;
   }, function(error){
     console.debug("Failure while fetching devices' list.");
   });
-
 });
+
+website.controller('navbarController', function($scope, $location) {
+      $scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+})
