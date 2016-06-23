3f7f17b92a07367cbe3b4853b59645d32bf83476
Thomas <t.raballand@gmail.com>
2016-06-23 15:27:21 +0900

    Style fenêtre feedback. Need link le back maintenant

---
+++ app/components/feedback/feedbackController.js
@@ -0,0 +1,55 @@
+website.controller('inscriptionController', function($scope, $http) {
+
+    $scope.inscriptionData = {
+      email : '',
+      pseudo : '',
+      password : '',
+    };
+
+    $scope.returnMessage = '';
+    $scope.loading;
+
+    $scope.inscriptionAction = function(){
+
+        var data = {
+          email : $scope.inscriptionData.email,
+          pseudo : $scope.inscriptionData.pseudo,
+          password : $scope.inscriptionData.password
+        };
+
+        if (data.password && data.pseudo && data.pseudo.length >= 3 && data.pseudo.length <= 16 && data.password.length >= 8) {
+
+          $scope.loading = true;
+
+          $http.post(url + '/api/registration', data)
+              .success(function(result) {
+
+                  if (result.Error == false) {
+                    $scope.inscriptionData = {};
+                    $scope.returnMessage = successMessage["INSCRIPTION"];
+                  } else {
+                    switch (result.Code)
+                    {
+                      case 100:
+                        $scope.returnMessage = errorMessage["INSCRIPTION_100"];
+                        break;
+                      case 101:
+                        $scope.returnMessage = errorMessage["INSCRIPTION_101"];
+                        break;
+                      case 104:
+                        $scope.returnMessage = errorMessage["INSCRIPTION_104"];
+                        break;
+                    }
+                  }
+
+                  $scope.loading = false;
+    
+              })
+              .error(function(result) {
+                  $scope.returnMessage = errorMessage["INSCRIPTION"];
+                  $scope.loading = false;
+          });
+              
+        }
+    };
+});
