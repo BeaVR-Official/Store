42f87a1a6db1f61942e73b3ecd2e9a50469ddeb9
Thomas <t.raballand@gmail.com>
2016-08-02 19:36:12 +0200

    Reset password

--- app/components/resetPassword/resetPasswordController.js
+++ app/components/resetPassword/resetPasswordController.js
@@ -23,30 +23,26 @@ website.controller('resetPasswordController', function($scope, $rootScope, $http
         $http.post(url + '/api/reset-password', data)
         .success(function(result){
 
-            if (result.Error == false) {
-                $scope.connectionData = {};
-                $scope.returnMessage = successMessage["MDPOUBLIE"];
-                returnMessageDiv.addClass("success-message");
-            } else {
-                switch (result.Code)
-                {
-                case 103:
-                    $scope.returnMessage = errorMessage["MDPOUBLIE_103"];
-                    break;
-                case 202:
-                    $scope.returnMessage = errorMessage["MDPOUBLIE_202"];
-                    break;
-                }
-                returnMessageDiv.addClass("error-message");
-            }
+            $scope.connectionData = {};
+            $scope.returnMessage = successMessage["MDPOUBLIE"];
+            returnMessageDiv.addClass("success-message");
             $scope.loading = false;
+
         })
         .error(function(result){
-            $scope.returnMessage = errorMessage["MDPOUBLIE"];
+
+            switch (result.error.status)
+            {
+            case 404:
+                $scope.returnMessage = errorMessage["MDPOUBLIE_404"];
+                break;
+            default:
+                $scope.returnMessage = errorMessage["MDPOUBLIE"];
+            }
             $scope.loading = false;
             returnMessageDiv.addClass("error-message");
+
         });
       };
 
-  console.log("Controller Reset Password");
 });
