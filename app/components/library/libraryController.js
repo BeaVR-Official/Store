f95daa6f64a65271d9a8b66cfc7596f8c7b10906
Nicolas Girard <nicolasg37@gmail.com>
2016-08-14 22:30:50 +0200

    debut de redesign sur library

--- app/components/library/libraryController.js
+++ app/components/library/libraryController.js
@@ -1,10 +1,16 @@
 website.controller('libraryController', function($scope, $rootScope, userData, AuthenticationService, libraryInfos) {
+    $scope.load = function() {
+      $('.special.cards .image').dimmer({
+        on: 'hover'
+      });
+    }
+    $scope.load();
     var userInfos = userData.data.data;
     $rootScope.menu = true;
     $rootScope.filterMenu = false;
     $rootScope.onlineMenu = true;
     $rootScope.offlineMenu = false;
-    $rootScope.profilePicture = userData.data.data.profile;
+    $rootScope.profilePicture = userData.data.data.picture;
     $rootScope.disconnect = AuthenticationService.disconnect;
     if (userInfos.rights.id == 2) {
       $rootScope.devMenu = true;
