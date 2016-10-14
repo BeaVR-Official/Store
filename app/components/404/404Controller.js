581b1d95134f4256c111b9eb16d98bab0987f19e
root <root@ns375152.ip-5-196-88.eu>
2016-10-14 21:47:14 +0200

    remove files

--- app/components/404/404Controller.js
+++
@@ -1,8 +0,0 @@
-website.controller('404Controller', function ($rootScope) {
-      $rootScope.menu = true;
-      $rootScope.homePage = false;
-      $rootScope.onlineMenu = false;
-      $rootScope.offlineMenu = false;
-      $rootScope.devMenu = false;
-      $rootScope.registerDev = false;
-});
--- app/components/500/500Controller.js
+++
@@ -1,8 +0,0 @@
-website.controller('500Controller', function ($rootScope) {
-      $rootScope.menu = true;
-      $rootScope.homePage = false;
-      $rootScope.onlineMenu = false;
-      $rootScope.offlineMenu = false;
-      $rootScope.devMenu = false;
-      $rootScope.registerDev = false;
-});
